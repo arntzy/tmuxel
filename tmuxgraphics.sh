89e3e9bc0a8f9ef0215587f1cff1d0afa15d4e07
Aaron Arntz <aarntz@gmail.com>
2015-02-21 18:22:08 -0500

    added vertical splits

--- tmuxgraphics.sh
+++ tmuxgraphics.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
-TODO: UPDATE TMUX ON THIS DROPLET SO YOU CAN resize panes
 
 tmux -2 new-session -d -n 'tmuxtest'
-#tmux new-window -n 'windowtest'
+panes=(0 1)
 
-for i in {0..5}; 
+#Horizontal splits
+for i in {0..20}; 
 	do
 	   tmux split-window -h -t $i 
+	   tmux select-layout even-horizontal
         done
 
- tmux resize-pane -t 0 -x 2
-#tmux split-window -h
-#tmux select-pane 
-tmux select-pane -t 0
+#tmux select-pane -t 0
+
+
+#for h in "${panes[@]}"
+#	do	
+#		echo $h
+#	done
+#tmux split-window -v -l 4 -d -t 0
+
+
+#for k in "${panes[@]}"
+for k in {0..21};
+	do
+	   for j in {0..12}; 
+		   do
+		       #tmux select-pane -t $k 
+		       tmux split-window -v -l 3 -t $k 
+		       #tmux select-layout even-vertical
+		   done
+	done
+
 tmux attach-session 
 
 
+#This is a test of my keylogging skills
 
 
-#split-window [-bdhvP] [-c start-directory] [-l size | -p percentage] [-t target-pane] [shell-command] [-F format]
+
+
+#split-window [-bdhvP] [-c start-directory] [-l size | -p percentage] [-t target-pane] [shell-command] [-F format] This is a test of a keystroke logger
