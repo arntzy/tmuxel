250e403b1fc5a7d6d2149240360ac0157cce4c7a
Aaron Arntz <aarntz@gmail.com>
2015-04-14 11:35:56 -0400

    python scripts added

--- tmuxgraphics.sh
+++ tmuxgraphics.sh
@@ -1,42 +1,86 @@
 #!/bin/bash
 
+#TODO LIST
+#Figure out how to divide evenly into the screen given dividing lines
+#Check for maximum number of windows
+#Supply usage documentation
+
 tmux -2 new-session -d -n 'tmuxtest'
-panes=(0 1)
+
+#Determine Terminal Window Size
+let lines=$(tput lines)
+let columns=$(tput cols)
+echo "Lines: " $lines
+echo "Columns: " $columns
+
+#Global Variables for the Splits
+let hsplits=$1
+let vsplits=$2
+let vchar=lines/vsplits
+let hchar=columns/hsplits
+let limit=(hsplits-1)*vsplits
+echo "hchar: $hchar"
+echo "vchar: $vchar"
+echo "limit: $limit"
 
 #Horizontal splits
-for i in {0..20}; 
+for((i=0;i<hsplits-1;i++))
 	do
-	   tmux split-window -h -t $i 
-	   tmux select-layout even-horizontal
-        done
-
-#tmux select-pane -t 0
-
-
-#for h in "${panes[@]}"
-#	do	
-#		echo $h
-#	done
-#tmux split-window -v -l 4 -d -t 0
-
-
-#for k in "${panes[@]}"
-for k in {0..21};
-	do
-	   for j in {0..12}; 
-		   do
-		       #tmux select-pane -t $k 
-		       tmux split-window -v -l 3 -t $k 
-		       #tmux select-layout even-vertical
-		   done
+	#echo $i 
+	#tmux select-pane -t $i
+	tmux split-window -d -h -l $hchar -t 0
+	#echo tmux split-window -h -l $hchar -t 0 
 	done
 
-tmux attach-session 
+#Probably not necessary
+#tmux select-layout even-horizontal
 
+#Vertical Splits
+for((j=0;j<=limit;j+=vsplits))
+	do
+	#echo $j
+	for((k=0;k<vsplits-1;k++))
+		do
+		#echo $k
+		if [ $j -eq 0 ]; then
+		let l=0
+		else
+		let l=j+k
+		fi
+		#echo l=$l
+		#tmux select-pane -t $j
+		#echo tmux select-pane -t $j
+		tmux split-window -v -l $vchar -t $l 
+		#echo tmux split-window -v -l $vchar -t $l 
+		done
+		#tmux select-layout even-vertical
+	done
 
-#This is a test of my keylogging skills
+#Build Export Strings
+#buildPS1(){
+	#let h=$1
+	#let v=$2
+	##let ps1="*"
+	#echo $(printf '*%.0s' {1..$h})
+#}
 
+#buildPS1 $hchar $vchar
 
+#let ps1=$(buildPS1 hchar vchar)
+#echo $ps1
 
+#Cycle through panes and export new prompt
+#let panes=hsplits*vsplits
 
-#split-window [-bdhvP] [-c start-directory] [-l size | -p percentage] [-t target-pane] [shell-command] [-F format] This is a test of a keystroke logger
+#for ((i=0;i<$panes;i++))
+	#do
+	##tmux display-message -pt 0 -F '#{pane_width}x#{pane_height}'
+	#tmux select-pane -t $i		
+	##export PS1="\033[01;32m\]\$"
+	##tmux -c "export PS1='***'"
+	#tmux send-keys -t $i 'export PS1="****"' Enter
+	#tmux send-keys -t $i 'clear' Enter
+	#done
+
+#Attach the session
+#tmux attach-session 
